72931ea92e212c1e64813fed3ff279f3cdef5cc0
xlz-44847 <1940629384@qq.com>
2024-01-27 21:25:07 +0800

    希尔排序博客插图

--- Sort/Sort/test.c
+++ Sort/Sort/test.c
@@ -4,7 +4,7 @@ int main()
 {
 	int a[] = { 2,6,5,8,4,1,3,7,9,0 };
 	int b[] = { 2,0,2,4,9,5,1,0,0,3,4,-5,-1,6,-3,-3,-1,-1,6,5,8,4,1,3,7,9,0 };
-	//InsertSort(a, sizeof(a) / sizeof(a[0]));
+	InsertSort(a, sizeof(a) / sizeof(a[0]));
 	//BubbleSort(a, sizeof(a) / sizeof(a[0]));
 	//ShellSort(a, sizeof(a) / sizeof(a[0]));
 	//SelectSort(a, sizeof(a) / sizeof(a[0]));
